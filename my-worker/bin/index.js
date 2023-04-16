7093ade8d8c4aba183bb4783d9613d4786886ff6
Justin <justintorre75@gmail.com>
2023-04-15 19:10:07 -0700

    worker

---
+++ my-worker/bin/index.js
@@ -0,0 +1,30 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+// src/index.ts
+const gpt3_tokenizer_1 = __importDefault(require("gpt3-tokenizer"));
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+app.use(express_1.default.text());
+app.post("/", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const text = req.body;
+    const tokenizer = new gpt3_tokenizer_1.default({ type: "gpt3" });
+    const encoded = tokenizer.encode(text);
+    const tokenCount = encoded.bpe.length;
+    res.send(tokenCount.toString());
+}));
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
---
+++ my-worker/src/index.ts
@@ -0,0 +1,23 @@
+// src/index.ts
+import GPT3Tokenizer from "gpt3-tokenizer";
+import express from "express";
+
+const app = express();
+app.use(express.text({ type: "text/plain" }));
+
+app.post("/", async (req, res) => {
+  const text = req.body;
+  if (typeof text !== "string") {
+    res.status(400).send("Bad request");
+    return;
+  }
+  const tokenizer = new GPT3Tokenizer({ type: "gpt3" });
+  const encoded: { bpe: number[]; text: string[] } = tokenizer.encode(text);
+  const tokenCount = encoded.bpe.length;
+  res.send(tokenCount.toString());
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
